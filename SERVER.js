3cc42044cb4566f58419fbd001539cec6d26e452
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-10-10 11:00:31 -0300

    updating school work2

--- SERVER.js
+++ SERVER.js
@@ -5,35 +5,30 @@ const PORT = 8080;
 
 app.use(express.json());
 
+function ValidaNumeros(pNumeros) {
+    
+    for(const numero of pNumeros){
+       
+       
+        const naoNumerico = pNumeros.find(item => typeof item !== `number`);
+
+        
+        if (naoNumerico !== undefined) {
+            throw new Error("Não é um número. Todos os valores devem ser numéricos para o cálculo.");
+            };
+        }
+
+
+        const resultado = pNumeros.reduce((acumulador, valorAtual) => acumulador + valorAtual);
+    return resultado;
+}
 
 app.post("/soma", (req, res) => {
     try {
         const { numeros } = req.body;
-
-
-        if (!Array.isArray(numeros) || numeros.length === 0) {
-            return res.status(400).json({
-                erro: "Dados inválidos. Por favor, envie um array de 'numeros'."
-            });
-        }
-
-
-        const naoNumerico = numeros.find(item => typeof item !== `number`);
-
-        if (naoNumerico == undefined) {
-
-            return res.status(400).json({
-                error: `O valor '${naoNumerico}' não é um número. Todos os valores devem ser numéricos para o cálculo.`
-            });
-        }
-
-
-        const resultado = numeros.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
-
-        res.status(200).json({
-            soma: resultado
-        });
-
+        const resultado = ValidaNumeros(numeros)
+        console.log(`o resultado da soma é ${resultado}`);
+        res.status(201).json({ result: `Dados recebidos com sucesso no servidor.` });
     } catch (error) {
         console.error("Erro ao processar a requisição:", error);
         res.status(500).json({
