b21cb4dee668ffd61efd12f75da1e1a1f8050dd4
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-10-16 09:23:22 -0300

    updating work4

--- SERVER.js
+++ SERVER.js
@@ -4,16 +4,18 @@ const PORT = 8080;
 
 app.use(express.json());
 
+function validarDados(pNome, pEmail, pSenha) {
+    if (!pnome || typeof pNome !== `string`) {
+        throw new Error("O nome tem que ser uma string não um numero!")
+    }
+    if (pnome().length > 3) {
+        throw new Error("insira pelo menos 3 letras em seu nome!");
+    }
+    if (!pEmail || typeof pEmail !== `string` || !pEmail.includes("@")) {
 
-app.post("/usuarios", (req,res) => { 
 
-    try {
-        
-    } catch (error) {
-        
     }
 
-});
 
 
 
@@ -66,19 +68,14 @@ app.post("/usuarios", (req,res) => {
 
 
 
+    app.use((req, res) => {
+        res.status(404).send(`pagina não encontrada!`)
+    })
 
 
-
-
-
-app.use((req, res) => {
-    res.status(404).send(`pagina não encontrada!`)
-})
-
-
-app.listen(PORT, () => {
-    console.log(` atividade 3 rodando em http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(` atividade 3 rodando em http://localhost:${PORT}`);
+    });
 
 
 
